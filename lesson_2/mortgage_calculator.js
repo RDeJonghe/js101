037cd04f6e2e4e92eb38b22efab75f52a7185b61
RDeJonghe <rdejonghe@hotmail.com>
2021-01-15 12:36:39 -0700

    messages edited

--- lesson_2/mortgage_calculator.js
+++ lesson_2/mortgage_calculator.js
@@ -31,69 +31,69 @@ GET perform another calculation
 */
 
 const readline = require('readline-sync'); // I think readline sync returns strings!!!
-const MESSAGES_DISPLAYED = require('./messages_displayed.json');
+const DISPLAY = require('./messages_displayed.json');
 
 function userMessages(messageDisplayed) {
   console.log(`>>> ${messageDisplayed}`);
 }
 
-userMessages(MESSAGES_DISPLAYED['language']);
+userMessages(DISPLAY['language']);
 let language = readline.question().replace(/['"]+/g, '').trim().toLowerCase().replace('glish', '').replace('pañol', '');
 // double check first replace for quotes.
 
 while (language !== 'en' && language !== 'es') {
-  userMessages(MESSAGES_DISPLAYED['language error']);
-  userMessages(MESSAGES_DISPLAYED['language']);
+  userMessages(DISPLAY['language error']);
+  userMessages(DISPLAY['language']);
   language = readline.question().trim().toLowerCase().replace('glish', '').replace('pañol', '');
 }
 
-userMessages(MESSAGES_DISPLAYED[language]['loan']);
+userMessages(DISPLAY[language]['loan']);
 let loanAmount = readline.question('$').replace('$', '').replace(',', '');
 loanAmount = parseFloat(loanAmount);
 
 while (Number.isNaN(loanAmount) || !Number.isInteger(loanAmount)) {
   if (Number.isNaN(loanAmount)) {
-    userMessages(MESSAGES_DISPLAYED[language]['entered NaN']);
-    userMessages(MESSAGES_DISPLAYED[language]['loan']);
+    userMessages(DISPLAY[language]['entered NaN']);
+    userMessages(DISPLAY[language]['loan']);
     loanAmount = parseFloat(readline.question('$').replace('$', '').replace(',', '')); // added .replace looks to work.
   } else if (!Number.isInteger(loanAmount)) {
-    userMessages(MESSAGES_DISPLAYED[language]['has decimal']);
+    userMessages(DISPLAY[language]['has decimal']);
     loanAmount = Math.round(loanAmount);
-    userMessages(`${MESSAGES_DISPLAYED[language]['rounded']} $${loanAmount}`);
+    userMessages(`${DISPLAY[language]['rounded']} $${loanAmount}`);
   }
 }
 
-userMessages(MESSAGES_DISPLAYED[language]['credit score']);
+userMessages(DISPLAY[language]['credit score']);
 let creditScore = parseFloat(readline.question());
 
 while (Number.isNaN(creditScore) || (creditScore < 300 || creditScore > 850) || !Number.isInteger(creditScore)) {
   if (Number.isNaN(creditScore)) {
-    userMessages(MESSAGES_DISPLAYED[language]['entered NaN']);
+    userMessages(DISPLAY[language]['entered NaN']);
     creditScore = parseFloat(readline.question());
   } else if (creditScore < 300 || creditScore > 850) {
-    userMessages(MESSAGES_DISPLAYED[language]['out of range']);
+    userMessages(DISPLAY[language]['out of range']);
     creditScore = parseFloat(readline.question());
   } else if (!Number.isInteger(creditScore)) {
     creditScore = Math.round(creditScore);
-    userMessages(`${MESSAGES_DISPLAYED[language]['credit score is']} ${creditScore}`);
+    userMessages(`${DISPLAY[language]['credit score is']} ${creditScore}`);
   }
 }
 
 let apr;
 if (creditScore < 630) {
-  apr = MESSAGES_DISPLAYED['credit']['bad'];
-  userMessages(`${MESSAGES_DISPLAYED[language]['apr message']} ${apr}%`);
+  apr = DISPLAY['credit']['bad'];
+  userMessages(`${DISPLAY[language]['apr message']} ${apr}%`);
 } else if (creditScore >= 630 && creditScore < 720) {
-  apr = MESSAGES_DISPLAYED['credit']['average'];
-  userMessages(`${MESSAGES_DISPLAYED[language]['apr message']} ${apr}%`);
+  apr = DISPLAY['credit']['average'];
+  userMessages(`${DISPLAY[language]['apr message']} ${apr}%`);
 } else if (creditScore >= 720) {
-  apr = MESSAGES_DISPLAYED['credit']['good'];
-  userMessages(`${MESSAGES_DISPLAYED[language]['apr message']} ${apr}%`);
+  apr = DISPLAY['credit']['good'];
+  userMessages(`${DISPLAY[language]['apr message']} ${apr}%`);
 }
 
-userMessages(MESSAGES_DISPLAYED[language]['loan duration']);
-let loanYears = parseFloat(readline.question('YEARS: '));
-let loanMonths = parseFloat(readline.question('MONTHS: '));
+userMessages(DISPLAY[language]['loan duration']);
+let loanYears = parseFloat(readline.question(`${DISPLAY[language]['pm4']}`));
+let loanMonths = parseFloat(readline.question(`${DISPLAY[language]['pm5']}`));
 
 while (
   isNaN(loanYears) ||
@@ -102,20 +102,20 @@ while (
   (loanYears === 0 && loanMonths === 0)
 ) {
   if (isNaN(loanYears)) {
-    userMessages(MESSAGES_DISPLAYED[language]['invalid year']);
+    userMessages(DISPLAY[language]['invalid year']);
     loanYears = parseFloat(readline.question('YEARS: '));
     loanMonths = parseFloat(readline.question('MONTHS: '));
   } else if (!Number.isInteger(loanYears)) {
-    userMessages(MESSAGES_DISPLAYED[language]['decimal year']);
+    userMessages(DISPLAY[language]['decimal year']);
     loanYears = parseFloat(readline.question('YEARS: '));
     loanMonths = parseFloat(readline.question('MONTHS: '));
   } else if (!Number.isInteger(loanMonths)) {
-    userMessages(MESSAGES_DISPLAYED[language]['invalid month']);
+    userMessages(DISPLAY[language]['invalid month']);
     loanYears = parseFloat(readline.question('YEARS: '));
     loanMonths = parseFloat(readline.question('MONTHS: ')); // don't need seperate decimal edge case for months, it just says invalid entry, no one is like 2.5 months the way you talk like 2.5 years.
   }
   if (loanYears === 0 && loanMonths === 0) {
-    userMessages(MESSAGES_DISPLAYED[language]['zero year and month']);
+    userMessages(DISPLAY[language]['zero year and month']);
     loanYears = parseFloat(readline.question('YEARS: '));
     loanMonths = parseFloat(readline.question('MONTHS: '));
   }
@@ -131,4 +131,24 @@ let monthlyInterestRate = (apr / 12) / 100;
 // prettier-ignore
 let monthlyPayment = loanAmount * (monthlyInterestRate / (1 - Math.pow((1 + monthlyInterestRate), (-loanDuration))));
 
-userMessages(`${MESSAGES_DISPLAYED[language]['payment message']} $${monthlyPayment.toFixed(2)}`);
+let totalPaid = monthlyPayment * loanDuration;
+let totalInterest = totalPaid - loanAmount;
+// prettier-ignore
+let percentInterest = (totalInterest / totalPaid) * 100;
+let percentPrincipal = loanAmount / totalPaid * 100;
+
+// prettier-ignore
+// userMessages(`${DISPLAY[language]['pm']} $${monthlyPayment.toFixed(2)} ${DISPLAY[language]['pm2']} $${loanAmount} ${DISPLAY     [language]['pm3']} ${loanYears} ${DISPLAY[language]['pm4']} ${loanMonths} ${DISPLAY[language]['pm5']}`);
+
+userMessages(`${DISPLAY[language]['pm']} $${monthlyPayment.toFixed(2)}.`);
+userMessages(`${DISPLAY[language]['pm2']} $${loanAmount}.`);
+userMessages(`${DISPLAY[language]['pm3']}`);
+userMessages(`${DISPLAY[language]['pm4']} ${loanYears}`);
+userMessages(`${DISPLAY[language]['pm5']} ${loanMonths}`);
+
+// prettier-ignore
+userMessages(`${DISPLAY[language]['total paid']} $${totalPaid.toFixed(2)}. ${DISPLAY[language]['total interest']} $${totalInterest.toFixed(2)}.`);
+
+// prettier-ignore
+
+userMessages(`${DISPLAY[language]['percent interest']} ${percentInterest.toFixed(0)}% ${DISPLAY[language]['percent principal']} ${percentPrincipal.toFixed(0)}%.`)
