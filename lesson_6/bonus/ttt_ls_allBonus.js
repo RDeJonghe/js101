f96db54ae5ee5caf472a786f18afe0c5de829b0a
RDeJonghe <rdejonghe@hotmail.com>
2021-05-09 16:19:38 -0600

    offenseDefense

--- lesson_6/bonus/ttt_ls_allBonus.js
+++ lesson_6/bonus/ttt_ls_allBonus.js
@@ -4,12 +4,20 @@
   // can do this like how they check for a winner
   // instead of checking for a winner I can check for two taken spaces and an empty space
 
+
+
+
 const readline = require("readline-sync");
 
 const INITIAL_MARKER = ' ';
 const HUMAN_MARKER = 'X';
 const COMPUTER_MARKER = 'O';
 const GAMES_NEEDED_TO_WIN_MATCH = 5;
+const WINNING_LINES = [
+  [1, 2, 3], [4, 5, 6], [7, 8, 9], // rows
+  [1, 4, 7], [2, 5, 8], [3, 6, 9], // columns
+  [1, 5, 9], [3, 5, 7]             // diagonals
+];
 
 function prompt(msg) {
   console.log(`=> ${msg}`);
@@ -81,10 +89,75 @@ function playerChoosesSquare(board) {
   board[square] = HUMAN_MARKER;
 }
 
+
+// defensive opportunity
+  // iterate over winning lines
+  // use array destructuring assignment to create variables
+  // check for human markers and initial markers
+  // if a defensive opportunity return String(variable) to get a string number to use
+
+function defensiveOpportunity(board) {
+  for (let line = 0; line < WINNING_LINES.length; line++) {
+    let [ sq1, sq2, sq3 ] = WINNING_LINES[line]
+      if (
+        board[sq1] === HUMAN_MARKER &&
+        board[sq2] === HUMAN_MARKER &&
+        board[sq3] === INITIAL_MARKER
+    ) {
+      return String(sq3);
+    } else if (
+        board[sq1] === INITIAL_MARKER &&
+        board[sq2] === HUMAN_MARKER &&
+        board[sq3] === HUMAN_MARKER
+    ) {
+      return String(sq1);
+    }
+  }
+  return null;
+}
+
+function offensiveOpportunity(board) {
+  for (let line = 0; line < WINNING_LINES.length; line++) {
+    let [ sq1, sq2, sq3 ] = WINNING_LINES[line]
+      if (
+        board[sq1] === COMPUTER_MARKER &&
+        board[sq2] === COMPUTER_MARKER &&
+        board[sq3] === INITIAL_MARKER
+    ) {
+      return String(sq3);
+    } else if (
+        board[sq1] === INITIAL_MARKER &&
+        board[sq2] === COMPUTER_MARKER &&
+        board[sq3] === COMPUTER_MARKER
+    ) {
+      return String(sq1);
+    }
+  }
+  return null;
+}
+
+
 function computerChoosesSquare(board) {
+  // if offensiveOpportunity
+    // let offenseSquare = offensiveOpportunity
+    // board[offensiveOpportunity] = computer marker
+  // if defensive opportunity
+    // let defenseSquare = defensiveOpportunity
+    // board[defensiveOpporunity] = computer marker
   let randomIndex = Math.floor(Math.random() * emptySquares(board).length);
   let square = emptySquares(board)[randomIndex];
-  board[square] = COMPUTER_MARKER;
+  if (offensiveOpportunity(board)) {
+    let offensiveSquare = offensiveOpportunity(board)
+    board[offensiveSquare] = COMPUTER_MARKER;
+  } else if (defensiveOpportunity(board)) {
+    let defensiveSquare = defensiveOpportunity(board);
+    board[defensiveSquare] = COMPUTER_MARKER;
+  } else if (emptySquares(board).includes('5')) {
+    board['5'] = COMPUTER_MARKER;
+  }
+  else {
+    board[square] = COMPUTER_MARKER;
+  }
 }
 
 function boardFull(board) {
@@ -96,14 +169,14 @@ function someoneWon(board) {
 }
 
 function detectWinner(board) {
-  let winningLines = [
-    [1, 2, 3], [4, 5, 6], [7, 8, 9], // rows
-    [1, 4, 7], [2, 5, 8], [3, 6, 9], // columns
-    [1, 5, 9], [3, 5, 7]             // diagonals
-  ];
+  // let winningLines = [ // MOVED TO GLOBAL CONSTANT VARIABLE
+  //   [1, 2, 3], [4, 5, 6], [7, 8, 9], // rows
+  //   [1, 4, 7], [2, 5, 8], [3, 6, 9], // columns
+  //   [1, 5, 9], [3, 5, 7]             // diagonals
+  // ];
 
-  for (let line = 0; line < winningLines.length; line++) {
-    let [ sq1, sq2, sq3 ] = winningLines[line];
+  for (let line = 0; line < WINNING_LINES.length; line++) {
+    let [ sq1, sq2, sq3 ] = WINNING_LINES[line];
 
     if (
         board[sq1] === HUMAN_MARKER &&
